2c508b14fb3ad91d59bace016dc8957ce60267cd
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2025-02-05 19:07:31 -0300

    boilerplate

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+<tr v-for="route in $router.options.routes">
+          <td>
+            <nuxt-link :to="route.path">{{route.path}}</nuxt-link>
+          </td>
+        </tr>
+</template>
