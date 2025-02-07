8cd4bb6f1cc5302803c71f21fab9ee16b1d3e6b6
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2025-02-07 20:17:04 -0300

    v2

--- app.vue
+++ app.vue
@@ -5,10 +5,10 @@
 </template>
 
 <script lang="ts">
-  import "bootstrap/js/index.umd.js"
+  //import "bootstrap/js/index.umd.js"
 </script>
 
 <style lang="scss">
   @use "bootstrap";
- // @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css");
+ @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css");
 </style>
