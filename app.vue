dc01867e7684c85710f7ded66e13165817d81227
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2025-02-07 20:14:56 -0300

    v2

--- app.vue
+++ app.vue
@@ -10,5 +10,5 @@
 
 <style lang="scss">
   @use "bootstrap";
-  @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css");
+ // @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css");
 </style>
